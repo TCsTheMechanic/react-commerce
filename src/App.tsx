bdd2f3d52d7f95f255451c2f6a46b6b8facf115e
TCsTheMechanic <ceruttioliveira@hotmail.com>
2021-08-06 12:51:34 -0300

    Config react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import Routes from './Routes'
+
+import GlobalStyle from './styles/global'
 
 function App() {
   return (
     <div className="App">
-
+      <GlobalStyle/>
+      <Routes/>
     </div>
   );
 }
---
+++ src/Routes.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Store from "./pages/Store";
+
+const Routes: React.FC = () => {
+  return (
+    <BrowserRouter>
+      <Route
+        exact path='/'
+        render={() => <Store/>}
+      />
+    </BrowserRouter>
+  )
+}
+
+export default Routes;

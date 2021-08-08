ade6f89341af9bd6eab52538e5cd63be9663286b
TCsTheMechanic <ceruttioliveira@hotmail.com>
2021-08-08 09:43:05 -0300

    Build first page and component example using styled components

---
+++ src/components/Product/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Container } from "./styles";
+
+const Product: React.FC = () => {
+  return (
+    <Container>
+      Oi fion
+    </Container>
+  )
+}
+
+export default Product

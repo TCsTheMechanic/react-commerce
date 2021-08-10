30f29e66ddedf36f56a1333e90bc609e3133ca7e
TCsTheMechanic <ceruttioliveira@hotmail.com>
2021-08-10 13:16:54 -0300

    Create and config styled components themes

---
+++ src/styles/themes/darkTheme.ts
@@ -0,0 +1,13 @@
+const darkTheme = {
+  title: 'darkTheme',
+
+  colors: {
+    primary: '#009CB6',
+    secondary: '7BC9D6',
+
+    background: '#181C23',
+    text: '#F5F5F5'
+  }
+}
+
+export default darkTheme
---
+++ src/styles/themes/lightTheme.ts
@@ -0,0 +1,13 @@
+const lightTheme = {
+  title: 'lightTheme',
+
+  colors: {
+    primary: '#009CB6',
+    secondary: '7BC9D6',
+
+    background: '#F5F5F5',
+    text: '#333'
+  }
+}
+
+export default lightTheme

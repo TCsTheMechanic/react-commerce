1769d09de31f1299a37c89265aa85913bfb22d34
TCsTheMechanic <ceruttioliveira@hotmail.com>
2021-08-10 15:15:05 -0300

    Add more styles to header

--- src/components/Header/headerStyles.ts
+++ src/components/Header/headerStyles.ts
@@ -3,6 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   display: flex;
   justify-content: space-between;
+  align-items: center;
+  padding: 0 1rem;
   background: ${ props => props.theme.colors.primary };
   width: 100%;
+  height: 3rem;
 `

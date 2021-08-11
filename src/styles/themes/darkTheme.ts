a9a239bafd426dcfb2bff07c3b7b848fc998b19b
TCsTheMechanic <ceruttioliveira@hotmail.com>
2021-08-11 16:50:44 -0300

    Fix color name

--- src/styles/themes/darkTheme.ts
+++ src/styles/themes/darkTheme.ts
@@ -3,7 +3,7 @@ const darkTheme = {
 
   colors: {
     primary: '#009CB6',
-    secondary: '7BC9D6',
+    secondary: '#7BC9D6',
 
     background: '#181C23',
     text: '#F5F5F5'
--- src/styles/themes/lightTheme.ts
+++ src/styles/themes/lightTheme.ts
@@ -3,7 +3,7 @@ const lightTheme = {
 
   colors: {
     primary: '#009CB6',
-    secondary: '7BC9D6',
+    secondary: '#7BC9D6',
 
     background: '#F5F5F5',
     text: '#333'

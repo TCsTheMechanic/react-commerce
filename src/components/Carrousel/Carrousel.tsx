0236a285a35b6ff4e32d18be51abcc5725e2fb8b
TCsTheMechanic <ceruttioliveira@hotmail.com>
2021-08-26 22:53:25 -0300

    Create carrousel component

---
+++ src/components/Carrousel/Carrousel.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { CarrouselContent } from './carrouselStyles';
+
+const Carrousel: React.FC = () => {
+  return (
+    <CarrouselContent>
+
+    </CarrouselContent>
+  );
+}
+
+export default Carrousel;
